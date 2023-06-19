c15f7c83a379e0bfdea7f38c279fe785c311128e
muller <pedromullergl11@gmail.com>
2023-06-19 19:03:13 -0300

    feat: endpoint get /talkers implemented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const { getAllTalkers } = require('./talkerUtils');
+// const anotherRouter = require('./routes/anotherRouter');
 const app = express();
 app.use(express.json());
+// app.use('/another', anotherRouter);
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
@@ -11,6 +13,15 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (req, res) => {
+  const response = await getAllTalkers();
+  if(!response) {
+    return res.status(400).json([]);
+  }
+  return res.status(200).json(response);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
+
---
+++ src/talker.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+// const { getAllTalkers } = require('./talkerUtils');
+// const app = express();
+
+// app.get('/talker', async (req, res) => {
+//   const response = await getAllTalkers();
+//   if(!response) {
+//     return [];
+//   }
+//   return res.status(200).json(response);
+// });
+
